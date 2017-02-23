d4840b459c10242125342998309d84a594276625
alexjst <alexjst@hotmail.com>
2017-02-23 23:24:11 +0800

    init java version of leetcode

--- C++/ScratchPaper/scratch.cpp
+++ C++/ScratchPaper/scratch.cpp
@@ -1,67 +1,62 @@
+#include <vector>
 #include <queue>
 #include <functional>
 #include <algorithm>
 #include <iostream>
+#include <stack>
+#include <string>
+#include <sstream>
+#include <unordered_map>
 
 using namespace std;
 
-class MedianFinder {
+class Solution {
 public:
+    string simplifyPath(string path) {
+        stack<string> pathStack;
+        int len = path.length();
+        if (len <= 1) return path;
 
-	// Adds a number into the data structure.
-	void addNum(int num) {
-		int leftSize = left.size();
-		int rightSize = right.size();
-
-		if (leftSize == 0) {
-			if (rightSize == 0) right.push(num);
-			else {
-				int rightTop = right.top();
-				if (num <= rightTop) left.push(num);
-				else {
-					right.pop();
-					left.push(rightTop);
-					right.push(num);
-				}
-			}
-		}
-		else {
-			int leftTop = left.top();
-			int rightTop = right.top();
-			if (leftSize == rightSize) {
-				if (leftTop <= num) right.push(num);
-				else {
-					left.pop(); left.push(num); right.push(leftTop);
-				}
-			}
-			else {
-				if (num <= rightTop) left.push(num);
-				else {
-					right.pop(); right.push(num); left.push(rightTop);
-				}
-			}
-		}
-	}
-
-	// Returns the median of current data stream
-	double findMedian() {
-		if (left.size() == right.size()) return (left.top() + right.top()) / 2.0;
-		else return (double)right.top();
-	}
-private:
-	priority_queue<int> left;
-	priority_queue<int, vector<int>, greater<int> > right;
+        for (int i = 0; i<len; i++) {
+            if (i == 0 || path[i] == '/') {
+                // find the next '/' or end of string
+                for (int j = i + 1; j <= len; j++) {
+                    if (j == len || path[j] == '/') {
+                        j--;
+                        int start = i + 1, end = j;
+                        if (end >= start) {
+                            string sub = path.substr(start, end - start + 1);
+                            if (sub.compare(".") == 0) {}
+                            else if (sub.compare("..") == 0) {
+                                if (pathStack.size()>0) pathStack.pop();
+                            }
+                            else {
+                                pathStack.push(sub);
+                            }
+                        }
+                        i = j; break;
+                    }
+                }
+            }
+        }
+        vector<string> pathVec;
+        while (!pathStack.empty()) {
+            pathVec.push_back(pathStack.top());
+            pathStack.pop();
+        }
+        reverse(pathVec.begin(), pathVec.end());
+        string result;
+        for (auto& p : pathVec) {
+            result = result + "/" + p;
+        }
+        if (result.empty()) result = "/";
+        return result;
+    }
 };
-
-
 
 int main()
 {
-	MedianFinder inst;
-	inst.addNum(2);
-	inst.addNum(1);
-	inst.addNum(3);
-	cout << inst.findMedian() << endl;
-
+    Solution s;
+    string result = s.simplifyPath("/a/./b/../../c/");
 	system("pause");
 }
