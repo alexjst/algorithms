21e55c6b66d210f3ea4b9ac38172da7087795c10
Alex Yang <alexyang@coupang.com>
2019-06-10 15:48:08 -0700

    [SQ-000] minor updates

---
+++ Java/src/DedupArray.java
@@ -0,0 +1,40 @@
+package alexyang.algorithms.Java.src;
+
+import java.util.List;
+
+public class DedupArray {
+    public static void main(String[] args) {
+       Integer[] a = {0, 0, 1, 1, 1, 2, 2, 3, 4, 5, 6, 6, 6, 6, 7}; 
+       dedup(a);
+       for (int i= 0; i< a.length; i++)  {
+           System.out.print(a[i]);
+       }
+    }
+
+    // in-place without allocation non-constant-size memory
+    protected static void dedup(Integer[] arr) {
+        if (arr == null || arr.length == 1) {
+            return;
+        }
+
+        int i = -1;
+        for (int j = 0; j < arr.length; j++) {
+            if (j==0 || arr[j] != arr[i]) {
+                arr[++i] = arr[j];
+                printArray(arr);
+                System.out.println(" ; i = " + i + "; j = " + j);
+            }
+        }
+    }
+
+    protected static void printArray(Integer[] arr) {
+       for (int i= 0; i< arr.length; i++) {
+           System.out.print(arr[i] + " ");
+       }
+    }
+}
+
+/**
+ * Array is simpler than List. Array's size is 'length' but List size is 'size()''
+ * For List, we use get(idx), and set(idx, element) to get and retrieve values.
+ */
