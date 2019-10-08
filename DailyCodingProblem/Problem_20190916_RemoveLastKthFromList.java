b564eb3f9629adb1a820ffb402de7be42bf9d85c
Alex Yang <alexyang@coupang.com>
2019-10-08 16:57:03 -0700

    [SQ-000] minor updates 2019-10-08 16:57:03

---
+++ DailyCodingProblem/Problem_20190916_RemoveLastKthFromList.java
@@ -0,0 +1,14 @@
+/**
+ * This problem was asked by Google.
+ * 
+ * Given a singly linked list and an integer k, remove the kth last element from
+ * the list. k is guaranteed to be smaller than the length of the list.
+ * 
+ * The list is very long, so making more than one pass is prohibitively
+ * expensive.
+ */
+
+ /**
+  * I'll omit this one since the suggested two-pointer solution is not really one pass
+  * in terms of cost number of pointer moves.
+  */
