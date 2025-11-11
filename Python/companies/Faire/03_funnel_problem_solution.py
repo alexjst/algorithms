80550820d2230905eab33ca9d273fd62dafd9cb9
Alex Yang <alexjst@hotmail.com>
2025-11-11 11:31:24 -0800

    Add full example to solution file comments

--- Python/companies/Faire/03_funnel_problem_solution.py
+++ Python/companies/Faire/03_funnel_problem_solution.py
@@ -6,6 +6,37 @@ Solution for Problem 3: Funnel Analysis
 
 TODO: Implement the Solution class with compute_funnel_counts method
 
+Example:
+    Funnels:
+        checkout_process,view_product,add_to_cart,enter_payment_info,complete_order
+        three_clicks_then_add,click_product,click_product,click_product,add_to_cart
+
+    Events:
+        4,1200,view_product
+        4,1210,add_to_cart
+        4,1300,enter_payment_info
+        5,1250,view_product
+        5,1260,add_to_cart
+        5,1270,enter_payment_info
+        5,1280,complete_order
+        6,2000,click_product
+        6,2005,click_product
+        6,2010,click_product
+        6,2015,add_to_cart
+        7,3000,click_product
+        7,3005,click_product
+
+    Output:
+        checkout_process,view_product(2),add_to_cart(2),enter_payment_info(2),complete_order(1)
+        three_clicks_then_add,click_product(2),click_product(2),click_product(1),add_to_cart(1)
+
+    Explanation:
+        - checkout_process: Users 4 and 5 both reach view_product, add_to_cart, enter_payment_info
+                           Only user 5 completes entire funnel
+        - three_clicks_then_add: User 6 does 3 click_product events then add_to_cart
+                                Users counted: step1(2), step2(2), step3(1), step4(1)
+                                User 6 reaches all 4 steps, User 7 only reaches first 2 clicks
+
 Algorithm hints:
 1. Parse funnels: split by comma to get funnel_name and list of steps
 2. Parse events: split by comma to get (user_id, timestamp, event_name)
